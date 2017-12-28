402851447e4c4efd508b6d24a2d4a67ce93200d1
johnmigliozzi <john.migliozzi@gmail.com>
2017-12-27 23:32:50 -0500

    - More CSS updates (finally happy with this version for now) - Changed which page names will show in the banner - Put banner HTML in a javascript file to help keep it consistent across pages - Created pages for main headers, including "More pages" - Create "Right Bar" for "More" Pages - created javascript file for "Right Bar" HTML to keep it consistent across pages

---
+++ banner.js
@@ -0,0 +1,15 @@
+document.write('\
+\
+<a href="index.html"><h1>John Migliozzi</h1></a>\
+<a href="more.html"><div class="Button">More</div></a>\
+<a href="programming.html"><div class="Button">Programming</div></a>\
+<a href="datascience.html"><div class="Button">Data Science</div></a>\
+<a href="excel.html"><div class="Button">Excel</div></a>\
+<a href="engineering.html"><div class="Button">Engineering</div></a>\
+<!--<a href="presentations.html"><div class="Button">Presentations</div></a>-->\
+\
+<div id="horizontal_rule"></div>\
+\
+Industrial Engineer\
+\
+');
---
+++ rightbar.js
@@ -0,0 +1,9 @@
+document.write('\
+\
+<a href="travel.html"><div class="Button">Travel</div></a>\
+<a href="guitar.html"><div class="Button">Guitar</div></a>\
+<a href="music.html"><div class="Button">Music</div></a>\
+<a href="finance.html"><div class="Button">Finance</div></a>\
+<a href="urbanism.html"><div class="Button">Urbanism</div></a>\
+\
+');
